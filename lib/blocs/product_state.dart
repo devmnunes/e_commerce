40da338c95d94c52f2a4fa98f65cc0dc20530bf9
Matheus Nunes <143001745+devmnunes@users.noreply.github.com>
2025-08-14 08:05:54 -0300

    commit

--- lib/blocs/product_state.dart
+++ lib/blocs/product_state.dart
@@ -1,14 +1,21 @@
 import 'package:e_commerce/models/product.dart';
+import 'package:flutter/widgets.dart';
 
 abstract class ProductState{
-  
+  final List<Product> products;
+
+  ProductState(this.products);
 }
 
-class ProductLoadingState extends ProductState {}
+class ProductLoadingState extends ProductState {
+  ProductLoadingState(super.products);
+}
 
 class ProductLoadedState extends ProductState {
   final List<Product> products;
-  ProductLoadedState(this.products);
+  ProductLoadedState(this.products) : super(products);
 }
 
-class ProductErrorState extends ProductState {}
+class ProductErrorState extends ProductState {
+  ProductErrorState(super.products);
+}
